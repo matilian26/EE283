c1d3c1ba16a88d20a65aae088210d4f293550e5a
Lily Li <matilian26@gmail.com>
2017-03-25 11:19:16 -0700

    changed output directory

--- Bioinformatics_Course/scripts/05alt_generatekundaje.sh
+++ Bioinformatics_Course/scripts/05alt_generatekundaje.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#$ -N generate_kundage
+#$ -N update_phredscores
 #$ -q bio
 #$ -t 1-24
 
 $prefix=`head -n $SGE_TASK_ID ATACseq.prefixes.txt | tail -n 1`
 
-echo "bds atac.bds -species dm6 -species_file ~/bio/lillyl1/ATACseq/KundajePipelineRefs/ -nth 8 -bwt2_idx /bio/lillyl1/ATACseq/KundajePipelineRefs/dm6/bowtie2_index/dm6.fa -fastq1_1 ${prefix}_F.fq.gz -fastq1_2 ${prefix}_R.fq.gz -out_dir /bio/lillyl1/ATACseq/processed_data/A4_ED_Rep2/ -gensz dm -chrsz /bio/lillyl1/ATACseq/KundajePipelineRefs/dm6/dm6.chrom.sizes -no_ataqc TRUE -title A4_ED_Rep2" >>kundaje_atac_code.txt
-
-
+echo "bds atac.bds -species dm6 -species_file ~/bio/lillyl1/ATACseq/KundajePipelineRefs/ -nth 8 -bwt2_idx /bio/lillyl1/ATACseq/KundajePipelineRefs/dm6/bowtie2_index/dm6.fa -fastq1_1 ${prefix}_F.fq.gz -fastq1_2 ${prefix}_R.fq.gz -out_dir /bio/lillyl1/ATACseq/processed_data/$(basename $prefix) -gensz dm -chrsz /bio/lillyl1/ATACseq/KundajePipelineRefs/dm6/dm6.chrom.sizes -no_ataqc TRUE -title $(basename $prefix)" >>kundaje_atac_code.txt
